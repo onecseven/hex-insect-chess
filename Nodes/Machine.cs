ce757fc42601e42b9c5c06183e53c232e5993565
Tati <iced.tati@gmail.com>
2024-01-15 16:36:12 -0300

    hasPlayerPlayedBee and mustPlayBee

--- Nodes/Machine.cs
+++ Nodes/Machine.cs
@@ -6,23 +6,7 @@ using System.Linq;
 
 public partial class Machine : Node
 {
-    //todo
-    //advance phase
-    //advance turn
-    /* Move validators 
-		0. The moves can only be emitted for players who match the turn color [x] (check if playerturn)
-		1. placement must check that the location in the Move
-	    only be adjacent to pieces that are the same color as
-	    the player who sent the move [x] (placementLegalityCheck)
-			1a. initial placement is the only placement that can start adjacent to the opposite color [x] (initialplacementlegality check)
-		2. move must be legal (as in the output of Piece.getLegalMoves must include the endpoint)
-			2a. move must have the correct origin for the piece
-			2b. each step in the path must abide by the freedom to move rule
-			2c. the move cannot at any point break the one hive rule
-		3. if the first three moves of the game for any particular player do not include a placement for the bee, then the only valid fourth move is bee placement
-		4. moves must check for wincon status after every move happens
-		5. after turn pass but before the game idles to receive moves, it must check that the player who's about to move has any legal moves. if they do not, then it autopasses.
-	 */
+    #region declarations
     Hive.Phases game_status = Hive.Phases.PREPARED;
     Hive.Players turn = Hive.Players.BLACK;
     List<Move> moves = new List<Move>();
@@ -36,7 +20,7 @@ public partial class Machine : Node
 
     [Signal]
     public delegate void PlacementEventHandler(Hive.PLACE move);
-
+    #endregion
 
     public void send_move(Move move)
     {
@@ -54,6 +38,21 @@ public partial class Machine : Node
         }
     }
 
+    #region validators
+    /* Move validators 
+		0. The moves can only be emitted for players who match the turn color [x] (check if playerturn)
+		1. placement must check that the location in the Move
+	    only be adjacent to pieces that are the same color as
+	    the player who sent the move [x] (placementLegalityCheck)
+			1a. initial placement is the only placement that can start adjacent to the opposite color [x] (initialplacementlegality check)
+		2. move must be legal (as in the output of Piece.getLegalMoves must include the endpoint)
+			2a. move must have the correct origin for the piece
+			2b. each step in the path must abide by the freedom to move rule
+			2c. the move cannot at any point break the one hive rule
+		3. if the first three moves of the game for any particular player do not include a placement for the bee, then the only valid fourth move is bee placement [x] hasPlayerPlayedBee and mustPlayBee
+		4. moves must check for wincon status after every move happens
+		5. after turn pass but before the game idles to receive moves, it must check that the player who's about to move has any legal moves. if they do not, then it autopasses.
+	 */
     bool checkIfPlayerTurn(Move move) => move.player == turn;
     bool placementLegalityCheck(Hive.PLACE move)
     {
@@ -74,6 +73,22 @@ public partial class Machine : Node
         }
         return false;
     }
+    bool hasPlayerPlayedBee(Hive.Players player)
+    {
+        var playerMoves = moves.Where(move => move.player == player).Where(move => move.type == MoveType.INITIAL_PLACE || move.type == MoveType.PLACE);
+        if (moves.Any(move => move.piece == Pieces.BEE)) return true;
+        return false;
+    }
+    bool mustPlayBee(Hive.Players player)
+    {
+        bool hasPlayedBee = hasPlayerPlayedBee(player);
+        bool hasPlayedThreeMoves = (moves.Where(move => move.player == player).ToList().Count) == 3;
+        bool belowLimit = (moves.Where(move => move.player == player).ToList().Count) > 3;
+        if (belowLimit && hasPlayedThreeMoves && !hasPlayedBee) return true;
+        else if (!belowLimit && !hasPlayedThreeMoves && !hasPlayedBee) throw new ArgumentException("illegal game state");
+        return false;
+    }
+    #endregion
     public bool wincon_check()
     {
         throw new NotImplementedException();
