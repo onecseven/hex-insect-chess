21c24fc5d2c13eb2e480c37b8c1615976d7f3324
Tati <iced.tati@gmail.com>
2024-04-01 18:09:25 -0300

    connected the textedit node that controls the button container and in the future will connect with the play/rewind buttons.

--- Nodes/NotationReader/TextEdit.cs
+++ Nodes/NotationReader/TextEdit.cs
@@ -1,11 +1,17 @@
 using Godot;
+using Hive;
+using Sylves;
 using System;
+using System.Collections.Generic;
 using static NotationReader;
 
 public partial class TextEdit : Godot.TextEdit
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+
+    [Export]
+    HFlowContainer container = null;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
 	}
 
@@ -15,7 +21,22 @@ public partial class TextEdit : Godot.TextEdit
 		{
 			var tokenized = NotationReader.Tokenize(Text);
 			var parsed = NotationReader.Parser(tokenized);
+			var translated = NotationReader.Translator(parsed);
+			int i = 1;
+            foreach (var token in tokenized)
+            {
+                var but = new Godot.Button();
+				but.Text = i + ". " + String.Join(" ", token);
+				but.Visible = true;
+                container.AddChild(but);
+				i++;
+            }
         }
+	}
+
+	public void populate(List<Hive.Move> moves)
+	{
+
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
