677f90034e32bdfc897c90d4e3ef3ff3299212d1
Tati <iced.tati@gmail.com>
2024-04-11 18:03:11 -0400

    new root node

---
+++ Nodes/Detached/GameWrapper.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class GameWrapper : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+
+	Hive.Hive machine = new Hive.Hive();
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
