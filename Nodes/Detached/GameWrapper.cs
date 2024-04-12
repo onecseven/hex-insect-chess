d26ff7e229cbd1dd8a2b6ec4be39c191ab10d4d0
Tati <iced.tati@gmail.com>
2024-04-12 14:56:10 -0400

    made the machine on gamewrapper public

--- Nodes/Detached/GameWrapper.cs
+++ Nodes/Detached/GameWrapper.cs
@@ -5,7 +5,7 @@ public partial class GameWrapper : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 
-	Hive.Hive machine = new Hive.Hive();
+	public Hive.Hive machine = new Hive.Hive();
 	public override void _Ready()
 	{
 	}
