b8351fc92753fb745409f09fd575cb469054b7f0
Tati <iced.tati@gmail.com>
2024-04-15 16:50:40 -0400

    added a tile background, and some indicators to differentiate multiple pieces

--- PiecePainter.cs
+++ PiecePainter.cs
@@ -5,11 +5,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using HexDemo3;
 
 public partial class PiecePainter : Node2D
 {
 
     GameWrapper _parent = null;
+
+    [Export]
+    public Font font = ThemeDB.FallbackFont;
 
     [Export]
     public GameWrapper parent {
@@ -24,6 +28,21 @@ public partial class PiecePainter : Node2D
     public TatiHex drawnGrid = null;
 
     public void onSuccessfulMoveHandler(Move move) => QueueRedraw();
+    private string dots(int i) {
+        switch (i)
+        {
+            case 0:
+                return "";
+            case 1:
+                return "∙";
+            case 2:
+                return "∶";
+            case 3:
+                return "∴";
+            default:
+                return "";
+        }
+    }
     public override void _Draw()
 	{
         if (parent == null)
@@ -33,8 +52,12 @@ public partial class PiecePainter : Node2D
         List<Tile> pieces = parent.machine.board.filteredPiecesInPlay.Select(cell => parent.machine.board.piecesInPlay[cell]).ToList();
         foreach (Tile tile in pieces)
         {
-                Vector3 center = drawnGrid.hexes[tile.cell];
-                DrawTexture(tile.activePiece.texture, (HiveUtils.Vector3ToVector2(center) + drawnGrid.Position) - new Vector2(16,16));
+            Color color = tile.activePiece.owner == Players.WHITE ? Color.FromHtml("d9a066") : Color.FromHtml("663931");
+            Vector2 center = HiveUtils.Vector3ToVector2(drawnGrid.hexes[tile.cell]) + drawnGrid.Position;
+            Vector2 offset = (tile.activePiece.type == Pieces.BEE || tile.activePiece.type == Pieces.LADYBUG || tile.activePiece.type == Pieces.MOSQUITO) ? new Vector2(16,16) : new Vector2(24,16);
+            DrawColoredPolygon(HexUtils.HexCornersFromCenter(center, drawnGrid._hexsize / 2, drawnGrid.orientation), color);
+            DrawTexture(tile.activePiece.texture, center - offset);
+            DrawString(ThemeDB.FallbackFont, center + new Vector2(8,12), $"{dots(tile.activePiece.id)}", HorizontalAlignment.Left, -1, 30, Color.FromHtml("000000"));
         }
-	}
+    }
 }
