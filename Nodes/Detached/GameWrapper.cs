47c856b07116c163de92c9b7a8df43e5c0d45681
Tati <iced.tati@gmail.com>
2024-04-16 18:00:33 -0400

    added restore/reset functions to make the state machine more interactable, also wrapped the internal state machine's "on move successful" event with an auto-subscribing one from gameWrapper

--- Nodes/Detached/GameWrapper.cs
+++ Nodes/Detached/GameWrapper.cs
@@ -1,17 +1,42 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class GameWrapper : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
+	public Hive.Hive _machine = new Hive.Hive();
+	public Hive.Hive machine { get => _machine; set {
+			_machine = value;
+            _machine.onSuccessfulMove += outerOnSuccessfulMove;
+		} }
 
-	public Hive.Hive machine = new Hive.Hive();
-	public override void _Ready()
+	public event Hive.Hive.onSuccessfulMoveEventHandler onOuterSuccessfulMove;
+    private void outerOnSuccessfulMove(Hive.Move move)
+    {
+		onOuterSuccessfulMove(move);
+    }
+
+    public override void _Ready()
 	{
-	}
+		machine.onSuccessfulMove += outerOnSuccessfulMove;
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+
+	public void reset ( )
+	{
+		machine = new Hive.Hive();
+	}
+    public void restore(List<Hive.Move>  moves)
+    {
+        machine = new Hive.Hive();
+        foreach (var move in moves)
+        {
+			machine.send_move(move);
+        }
+    }
 }
